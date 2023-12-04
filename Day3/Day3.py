631de80a245e59ab4a5cbdc86929eda11ef5664b
Aditya Khowal <adityakh2003@outlook.com>
2023-12-03 23:48:49 -0800

    day 3

--- Day3/Day3.py
+++ Day3/Day3.py
@@ -18,8 +18,9 @@ def part1(fileName):
                 end = j
             
             else:
-                #numbs.append(pp[i][start:end+1])
-                if is_symbol(pp[i][j]) or j == len(pp[i])-1:
+
+                print(pp[i][start:end+1])
+                if is_symbol(pp[i][j]) or j <= len(pp[i])-1:
                     if start  != -1 and end !=-1:
                         
                         numbs.append(pp[i][start:end+1])
@@ -38,20 +39,31 @@ def is_symbol(char):
         return False
     return not char.isdigit()
 
-
 def nextToSymbol(schematic, i, start, end):
     rows = len(schematic)
     cols = len(schematic[0])
     directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
     for j in range(start, end+1):
         for dx, dy in directions:
-            x =i+dx
-        #for j in range( start, end+1):
-            y= j +dy 
-           #print(schematic[i:dx][y:dy])
-            if 0<=x<=rows and 0<=y<=cols and is_symbol(schematic[x][y]):
+            x = i + dx
+            y = j + dy 
+            if 0 <= x < rows and 0 <= y < cols and is_symbol(schematic[x][y]):
                 return True
     return False
+
+#def nextToSymbol(schematic, i, start, end):
+#    rows = len(schematic)
+#    cols = len(schematic[0])
+#    directions = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+#    for j in range(start, end+1):
+#        for dx, dy in directions:
+#            x =i+dx
+#        #for j in range( start, end+1):
+#            y= j +dy 
+#           #print(schematic[i:dx][y:dy])
+#            if 0<=x<=rows and 0<=y<=cols and is_symbol(schematic[x][y]):
+#                return True
+#    return False
 
 
 def symbolSearch():
@@ -63,4 +75,4 @@ def symbolSearch():
 
 
 
-print(part1('Day3/inputSmall.txt'))
+print(part1('./input.txt'))
